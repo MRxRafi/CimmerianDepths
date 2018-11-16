68395f7517dc53016709893ab0acf5d46194a6a0
Pistoncito <victorglezrivera@hotmail.com>
2018-11-16 18:17:18 +0100

    Update playerSelect.js

--- Cimmerian Depths/src/states/playerSelect.js
+++ Cimmerian Depths/src/states/playerSelect.js
@@ -2,6 +2,16 @@ CimmerianDepths.playerSelectState = function(game) {
 
 }
 
+ function changeToSelected(phaserText)
+    {
+        //phaserText.fontStyles= this.selectedTextStyle;
+    }
+
+    function changeToUnselected(phaserText)
+    {
+        phaserText.fontStyles= this.unselectedTextStyle;
+    }
+
 CimmerianDepths.playerSelectState.prototype = {
 
     preload: function() {
@@ -9,8 +19,17 @@ CimmerianDepths.playerSelectState.prototype = {
     },
 
     create: function() {
-    	game.state.start('equipmentState');
-    	//game.state.start('dungeonState');
+         this.unselectedTextStyle= {fill:"rgb(155,155,155)", font: "40px Impact", align:"center"};
+        this.selectedTextStyle= {fill:"rgb(255,255,255)", font: "40px Impact", align:"center"};
+
+        this.equipar= new createButton(100, game.height*0.66, "EQUIPAR", this.unselectedTextStyle,
+         function(){game.state.start("equipmentState")});
+         //this.equipar.setOnClick(changeToSelected(this.equipar));
+
+         this.jugar= new createButton(game.width/2, game.height-30, "JUGAR", this.unselectedTextStyle);
+        
+        
+        
     },
 
     update: function() {
