ca887055eb6cf79c33d3078b3be51db59db68c6b
Rafael <38105703+MRxRafi@users.noreply.github.com>
2018-11-16 19:38:34 +0100

    Añadidas animaciones al moverse

--- Cimmerian Depths/src/objects/jugador.js
+++ Cimmerian Depths/src/objects/jugador.js
@@ -8,6 +8,11 @@ function Jugador(x, y, sprsheet){
     var downAnimation = this.sprite.animations.add('walkDown', [20, 21, 22, 23, 24, 25, 26, 27, 28]);
     var rightAnimation = this.sprite.animations.add('walkRight', [30, 31, 32, 33, 34, 35, 36, 37, 38]);
 
+    var up = false; /* Estas variables sirven para administrar que animación debe ir y si está o no activada */
+    var down = false;/* Por defecto, todas desactivadas */
+    var left = false;
+    var right = false;
+
     //Creamos controles del jugador
     this.createInputs = function () {
         this.wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
@@ -26,13 +31,25 @@ function Jugador(x, y, sprsheet){
             this.sprite.body.velocity.x = 230;
         }
         if (this.wKey.isDown) {
-            //this.up = true;
+            up = true;
             this.sprite.body.velocity.y = -230;
         }
         if (this.sKey.isDown) {
-            //this.down = true;
+            down = true;
             this.sprite.body.velocity.y = 230;
         }
+
+        //Activamos animaciones left o right
+        if (this.aKey.isDown && !this.wKey.isDown && !this.sKey.isDown) { left = true; }
+        if (this.dKey.isDown && !this.wKey.isDown && !this.sKey.isDown) { right = true; }
+    }
+
+    this.updateAnimations = function () {
+        if (up) { this.sprite.animations.play('walkUp', 30, true); } else { this.sprite.animations.stop('walkUp', true); }
+        if (down) { this.sprite.animations.play('walkDown', 30, true); } else { this.sprite.animations.stop('walkDown', true); }
+        if (left) { this.sprite.animations.play('walkLeft', 30, true); } else { this.sprite.animations.stop('walkLeft', true); }
+        if (right) { this.sprite.animations.play('walkRight', 30, true); } else { this.sprite.animations.stop('walkRight', true); }
+        up = down = right = left = false; //Reiniciamos variables
     }
 
     //Activamos físicas arcade para el personaje
