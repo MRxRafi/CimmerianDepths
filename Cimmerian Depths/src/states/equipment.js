01efc7247968e40147e3a7c05be0f562c6989b32
Pistoncito <victorglezrivera@hotmail.com>
2018-11-21 17:34:39 +0100

    Update equipment.js

--- Cimmerian Depths/src/states/equipment.js
+++ Cimmerian Depths/src/states/equipment.js
@@ -1,16 +1,31 @@
 CimmerianDepths.equipmentState = function(game) {
-
+    //Texts
+this.recetasText;
+this.forjadosText;
+this.equipamientoText;
+this.returnText;
+this.style;
 }
 
 CimmerianDepths.equipmentState.prototype = {
 
     preload: function() {
-        
     },
 
     create: function() {
-    	game.state.start('dungeonState');
-    	//game.state.start('playerSelectState');
+        game.add.image(0,0,'titleScreenBG');
+
+        this.style= { font: "30px Averia Sans Libre", fill: "rgb(155,155,155)", align: "center" };
+       
+       this.recetasText= new createButton(game.width*0.33,100,"Recetas",this.style);
+
+       this.forjadosText= new createButton(game.width*0.33,250,"Forjados",this.style);
+
+       this.equipamientoText=new createButton(game.width*0.33,400,"Equipamiento",this.style);
+
+       this.returnText= new createButton(game.width-100,50,"Volver",this.style,
+         function(){game.state.start("playerSelectState")});
+
     },
 
     update: function() {
