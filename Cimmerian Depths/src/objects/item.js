64332e1e522da68d592e12cd73dc4c22fd862ed4
Rafael <38105703+MRxRafi@users.noreply.github.com>
2018-11-22 23:55:09 +0100

    Update item.js

--- Cimmerian Depths/src/objects/item.js
+++ Cimmerian Depths/src/objects/item.js
@@ -1,13 +1,7 @@
 function Item(num){
 
-    var numero = num;
+    this.numero = num;
     this.sprite = game.add.sprite(500,700,'bs');
-
-    this.recoger = function(player){
-        player.objeto += numero;
-        this.sprite.destroy();
-        delete this;
-    }
 
     //Activamos Physics
     game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
