d36d6422f7713e08ad154449003b170257c59857
Pistoncito <victorglezrivera@hotmail.com>
2018-11-27 20:04:15 +0100

    nuevo objeto icon

--- Cimmerian Depths/src/objects/Icon.js
+++ Cimmerian Depths/src/objects/Icon.js
@@ -1,120 +1,70 @@
-function Icon(x,y,sprite_name)
-{
-    this.stored= false;
-    this.initX= x;
-    this.initY= y;
+function Icon(x, y, arrayRow, arrayCol, sprite_name, storedIn, typeOf) {
+    this.storedIn = storedIn;
+    this.typeOf = typeOf;
+    this.initX = x;
+    this.initY = y;
+    this.arrayRow = arrayRow;
+    this.arrayCol = arrayCol;
+    this.sprite_name = sprite_name;
 
-    this.background= game.add.sprite(x,y, 'iconBg');
+    this.background = game.add.sprite(x, y, 'iconBg');
     game.physics.enable(this.background, Phaser.Physics.ARCADE);
-    this.background.inputEnabled= true;
-    this.background.alpha= 0.6;
-    this.background.anchor.x= 0.5;  this.background.anchor.y= 0.5;
-    this.background.body.inmovable=true;
+    this.background.inputEnabled = true;
+    this.background.alpha = 0.6;
+    this.background.anchor.x = 0.5; this.background.anchor.y = 0.5;
+    this.background.body.inmovable = true;
+
+
+
     this.sprite;
 
-    if(sprite_name != undefined)
-    {
-        this.sprite= game.add.sprite(x,y,sprite_name);
+    this.setFromUndefined = function (newSprite) {
+        if (newSprite != undefined) {
+            this.sprite = newSprite;
+            game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+            iconsGroup.add(this.sprite);
+            this.sprite.inputEnabled = true;
+            this.sprite.alpha = 0.6;
+            this.sprite.input.enableDrag(true);
+            this.sprite.anchor.x = 0.5; this.sprite.anchor.y = 0.5;
 
-        game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
-        iconsGroup.add(this.sprite);
-        this.sprite.inputEnabled= true;
-        this.sprite.alpha= 0.6;
-        this.sprite.input.enableDrag(true);
+            this.sprite.events.onInputOut.add(function () {
+                if (this.sprite != undefined)
+                    this.sprite.alpha = 0.6
+            }, this);
 
-    
-        this.sprite.anchor.x= 0.5;  this.sprite.anchor.y= 0.5;
-   
+            this.sprite.events.onInputOver.add(function () {
+                if (this.sprite != undefined)
+                    this.sprite.alpha = 1.0
+            }, this);
 
 
+        }
+    }
 
-  
 
-    this.sprite.events.onInputOut.add(function()
-    {
-        if(this.sprite !=undefined)
-        this.sprite.alpha = 0.6
-    }, this);
-    this.sprite.events.onInputOver.add(function()
-    {
-        if(this.sprite !=undefined)
-        this.sprite.alpha = 1.0
-    }, this);
-    this.sprite.events.onDragStop.add(
-        function()
-        {
-
-var whereToLook;
-            if(!this.stored)
-            {
-                whereToLook= personaje.cajonRecipes;
-            }else
-                {
-                    whereToLook= personaje.recipeInventory;
+    if (sprite_name != undefined) {
+        this.setFromUndefined(game.add.sprite(this.initX, this.initY, sprite_name));
+        this.sprite.events.onDragStop.add(
+            function () {
+                var swap = false;
+                swap = personaje.swapIfNeeded(this.arrayRow, this.arrayCol, this.sprite_name, this.storedIn, this.typeOf);
+                if (swap === false) {
+                    this.sprite.x = this.initX;
+                    this.sprite.y = this.initY;
+                    this.sprite.alpha = 0.6;
                 }
 
-if(!this.stored)
-{
- //Miro si colisiona con algun objeto del array de recipes del personaje
- if(whereToLook)
- {
-  var collision=false;
-  var xPos;
-  var yPos;
-  for(i=0; i < whereToLook.rows; i++)
-  {
-      for(j=0; j < whereToLook.cols; j++)
-      {
-          if(game.physics.arcade.collide(this.sprite, whereToLook.icons[i][j].background))
-          {
-              collision=true;
-              xPos=i;
-              yPos=j;
-          }
-          
-          if(collision===true)break;
-      }
-      if(collision===true)break;
-  }
- }
-  if(xPos != undefined && yPos != undefined)
-  var swapped =personaje.swapRecipes(xPos, yPos, this.sprite, whereToLook);
-  if(swapped) { this.switch();}
-  if(this.sprite !=undefined)
-  {
-      this.sprite.x= this.initX;
-      this.sprite.y= this.initY;
-      this.sprite.alpha = 0.6;
-  }
-}
-       
-
-        }, this);
-  
+            }, this);
     }
 
-this.switch= function()
-{
-    if(this.stored) this.stored=false;
-    else this.stored=true;
-}
-this.clean= function()
-{
-    this.background.destroy();
-    this.sprite.destroy();
-}
 
-this.setToInterfazCoords= function(x,y)
-{
-    if(this.sprite != undefined)
-    {
-        this.sprite.x= x;
-        this.sprite.y= y;
-        this.sprite.alpha = 0.6;
+
+    this.clear = function () {
+        this.background.destroy();
+        if(this.sprite)
+        this.sprite.destroy();
     }
-        this.background.x= x;
-        this.background.y= y;
-        
-}
+
 
 }
